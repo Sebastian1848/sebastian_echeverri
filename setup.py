3d6764d83d1e9911ea8fca483c41aa2bf2187aea
Sebastian1848 <sebastian.echeverri@est.iudigital.edu.co>
2025-02-28 21:12:34 +0000

    prueba api countries

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, findpackages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="SEbastián Echeverri",
+    author_email="",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "matplotlib"
+    ]
+    
+)
---
+++ src/pad/actividad1.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+class Ingestiones: 
+    def __init__(self):
+        self.ruta_static = "src/pad/static/"  
+
+    def leer_api(self, url):
+        response = requests.get(url)
+        return response.json()
+
+    def escribir_json(self):
+        pass  
+
+# crear instancia de clase Ingestiones 
+ingestion = Ingestiones()
+
+# URL de API que no necesita autenticación 
+url_api = "https://restcountries.com/v3.1/all"  
+
+# con esta linea se obtiene la información de la API
+datos_json = ingestion.leer_api(url_api)
+
+# estas lineas imprimen los datos obtenidos
+print("Esta es la ruta estática:", ingestion.ruta_static)
+print("Datos JSON obtenidos:", json.dumps(datos_json, indent=4, ensure_ascii=False))
